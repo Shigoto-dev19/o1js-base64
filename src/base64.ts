4d8999601d273036dee28a9872056398e77d8942
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2024-05-06 11:50:48 +0100

    Refine code readability

--- src/base64.ts
+++ src/base64.ts
@@ -1,18 +1,17 @@
-//TODO Check that input length is multiple of 4
-//TODO Add a generic way to calculate byte length
-//TODO Make generic given ByteLength
-//TODO Add README 
-//TODO Fix Notations
-
-import { Field, Bool, Provable } from 'o1js';
+import { Field, Bool, Bytes, assert } from 'o1js';
 
 export { base64Decode };
 
-function base64Decode(input: Field[]) {
-  const byteLength = 32;
-  const charLength = input.length;
-  // const byteLength = 4 * ((byteLength + 2) / 3); // 4 chars encode 3 bytes
-  let out: Field[] = [];
+function base64Decode(inputBytes: Bytes, byteLength: number) {
+  const encodedB64Bytes = inputBytes.toFields();
+
+  const charLength = encodedB64Bytes.length;
+  assert(
+    charLength % 4 === 0,
+    'Input base64 byte length should be a multiple of 4!'
+  );
+
+  let decodedB64Bytes: Field[] = [];
 
   let bitsIn: Bool[][][] = Array.from({ length: charLength / 4 }, () => []);
   let bitsOut: Bool[][][] = Array.from({ length: charLength / 4 }, () =>
@@ -22,11 +21,11 @@ function base64Decode(input: Field[]) {
   let idx = 0;
   for (let i = 0; i < charLength; i += 4) {
     for (let j = 0; j < 4; j++) {
-      const translated = base64Lookup(input[i + j]);
+      const translated = base64Lookup(encodedB64Bytes[i + j]);
       bitsIn[i / 4][j] = translated.toBits(6);
     }
 
-    // Do the re-packing from four 6-bit words to three 8-bit words.
+    // Convert from four 6-bit words to three 8-bit words, unpacking the base64 encoding
     bitsOut[i / 4][0] = [
       bitsIn[i / 4][1][4],
       bitsIn[i / 4][1][5],
@@ -46,63 +45,60 @@ function base64Decode(input: Field[]) {
 
     for (let j = 0; j < 3; j++) {
       if (idx + j < byteLength) {
-        out[idx + j] = Field.fromBits(bitsOut[i / 4][j]);
+        decodedB64Bytes[idx + j] = Field.fromBits(bitsOut[i / 4][j]);
       }
     }
     idx += 3;
   }
 
-  return out;
+  return decodedB64Bytes;
 }
 
+// Adapted from the algorithm described in: http://0x80.pl/notesen/2016-01-17-sse-base64-decoding.html#vector-lookup-base
 function base64Lookup(input: Field): Field {
-  // Any check
-  let oddRangeCheck = Field(0);
+  // A variable to check if the input consists solely of valid base64 characters
+  let isValidBase64Chars = Field(0);
 
   // ['A', 'Z']
   let le_Z = input.lessThan(Field(91 + 1));
   let ge_A = input.greaterThan(Field(65 - 1));
-
   let range_AZ = le_Z.and(ge_A);
-  let sum_AZ = Provable.if(range_AZ, input.sub(65), Field(0));
-  oddRangeCheck = oddRangeCheck.add(range_AZ.toField());
+  let sum_AZ = range_AZ.toField().mul(input.sub(65));
+  isValidBase64Chars = isValidBase64Chars.add(range_AZ.toField());
 
   // ['a', 'z']
   let le_z = input.lessThan(Field(122 + 1));
   let ge_a = input.greaterThan(Field(97 - 1));
-
   let range_az = le_z.and(ge_a);
-  let sum_az = Provable.if(range_az, input.sub(71), Field(0));
-  sum_az = sum_az.add(sum_AZ);
-
-  oddRangeCheck = oddRangeCheck.add(range_az.toField());
+  let sum_az = range_az.toField().mul(input.sub(71)).add(sum_AZ);
+  isValidBase64Chars = isValidBase64Chars.add(range_az.toField());
 
   // ['0', '9']
   let le_9 = input.lessThan(Field(57 + 1));
   let ge_0 = input.greaterThan(Field(48 - 1));
-
   let range_09 = le_9.and(ge_0);
-  let sum_09 = Provable.if(range_09, input.add(4), Field(0));
-  sum_09 = sum_09.add(sum_az);
-  oddRangeCheck = oddRangeCheck.add(range_09.toField());
+  let sum_09 = range_09.toField().mul(input.add(4)).add(sum_az);
+  isValidBase64Chars = isValidBase64Chars.add(range_09.toField());
 
   // '+'
   let equal_plus = input.equals(43);
-  let sum_plus = Provable.if(equal_plus, input.add(19), Field(0));
-  sum_plus = sum_plus.add(sum_09);
-  oddRangeCheck = oddRangeCheck.add(equal_plus.toField());
+  let sum_plus = equal_plus.toField().mul(input.add(19)).add(sum_09);
+  isValidBase64Chars = isValidBase64Chars.add(equal_plus.toField());
 
   // '/'
   let equal_slash = input.equals(47);
-  let sum_slash = Provable.if(equal_slash, input.add(16), Field(0));
-  sum_slash = sum_slash.add(sum_plus);
-  oddRangeCheck = oddRangeCheck.add(equal_slash.toField());
+  let sum_slash = equal_slash.toField().mul(input.add(16)).add(sum_plus);
+  isValidBase64Chars = isValidBase64Chars.add(equal_slash.toField());
 
   // '='
   let equal_eqsign = input.equals(61);
-  oddRangeCheck = oddRangeCheck.add(equal_eqsign.toField());
+  isValidBase64Chars = isValidBase64Chars.add(equal_eqsign.toField());
 
-  oddRangeCheck.assertEquals(1, 'Something with lookup went wrong!');
+  // Validate if input contains only valid base64 characters
+  isValidBase64Chars.assertEquals(
+    1,
+    'Please provide base64-encoded bytes containing only alphanumeric characters and +/='
+  );
 
   return sum_slash;
 }
